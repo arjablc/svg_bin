83499a98c9d51a68c2aa3c3d11c4e5ded20d3dc7
arjablc <arjablc@gmail.com>
2025-03-23 11:28:33 +0545

    checkpoint( c and p arg)

--- bin/svg_bin.dart
+++ bin/svg_bin.dart
@@ -1,5 +1,32 @@
-import 'package:svg_bin/src/create_bin.dart';
+import 'dart:io';
 
-Future<void> main() async {
-  await generate();
+import 'package:svg_bin/src/create_bin.dart';
+import 'package:args/args.dart';
+import 'package:svg_bin/src/constants.dart';
+
+Future<void> main(List<String> args) async {
+  final parser = ArgParser()
+    ..addFlag(
+      'cat',
+      abbr: 'c',
+      defaultsTo: false,
+      negatable: false,
+      help: "whether to create category classes or not",
+    )
+    ..addOption('path',
+        abbr: 'p',
+        defaultsTo:
+            "$defaultAssetFolder${Platform.pathSeparator}$defaultAssetFile",
+        help: "The file where the generated path class is kept");
+
+  try {
+    final ArgResults res = parser.parse(args);
+  } catch (e) {
+    stderr
+      ..write(e.toString())
+      ..writeln("Args error")
+      ..writeln()
+      ..write(parser.usage);
+  }
+  //await generate();
 }
---
+++ lib/src/constants.dart
@@ -0,0 +1,2 @@
+const String defaultAssetFolder = "lib/assets/";
+const String defaultAssetFile = "app_asset.dart";
